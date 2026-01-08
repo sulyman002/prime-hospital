b1b2cf9c6165e9c3c27580d6894f6b0a28003bf3
sulyman002 <oyedelesulaiman@gmail.com>
2026-01-08 10:52:13 +0000

    completed login form

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -12,30 +12,35 @@ const page = () => {
           </div>
           <button className="button">New here? Setup your practice</button>
         </div>
-      <div className="flex flex-col">
-        <div className="flex items-center gap-3">
-          {/* key */}
-          <div className="flex items-center justify-center p-1 bg-[#DFE8FC]">
-            a
+        <div className="flex flex-col">
+          <div className="flex items-center gap-3">
+            {/* key */}
+            <div className="flex items-center justify-center p-1 bg-[#DFE8FC]">
+              a
+            </div>
+            <p className="text-primary font-700 text-1xl">
+              Login
+            </p>
           </div>
-          <p className="text-primary font-700 text-1xl">
-            Login
-          </p>
+          <hr className="h-1 bg-[#E6E8EC] w-full" />
+
+          <form className="flex flex-col gap-4 w-full">
+            <div className="flex flex-col gap-1 w-full">
+              <label htmlFor="email" className="text-tertiary">Email address</label>
+              <input type="email" name="" id="email" className=" rounded-[5px] " />
+            </div>
+
+            <div className="flex flex-col gap-1 w-full">
+              <label htmlFor="password" className="text-tertiary">Password</label>
+              <input type="password" name="" id="password" className=" rounded-[5px] " />
+            </div>
+
+          </form>
+
         </div>
-        <hr className="h-1 bg-[#E6E8EC] w-full" />
-
-        <form className="flex flex-col gap-4 w-full">
-          <div className="flex flex-col gap-1 w-full">
-            <label htmlFor="email" className="text-tertiary">Email address</label>
-            <input type="email" name="" id="email" className=" rounded-[5px] " />
-          </div>
-
-        </form>
-
-      </div>
       </div>
     </section>
-  ) 
+  )
 }
 
 export default page
